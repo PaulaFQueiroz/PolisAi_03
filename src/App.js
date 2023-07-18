94f9d650500fc6c6b00c8d95d09fd7aca3e350aa
Paula Queiroz <anapauladequeiroz@my.smccd.edu>
2023-07-18 13:12:04 -0700

    up to date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+// pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+// components
+import Navbar from './components/Navbar';
+
+const theme = createTheme({
+
+
+  
+  palette: {
+    primary: {
+      light: '#7986cb',
+      main: '#5c6bc0',
+      dark: '#3f51b5'
+
+    },
+    secondary: {
+      light: '#ffefbc',
+      main: '#ffd966',
+      dark: '#ffd042'
+
+    }
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+
+
+
+/*import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 
 
@@ -32,3 +90,64 @@ import Navbar from './components/Navbar';
     }
     
     export default App;
+
+
+    
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+
+
+// pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+
+//components
+import Navbar from './components/Navbar';
+
+const theme = createTheme ({
+  palette: {
+    primary: {
+    light: 'a33c9dc',
+    main: 'a00bcd4',
+    dark: 'a008394',
+    contrastText: 'afff'
+    },
+    secondary: {
+      light: 'aff6333',
+      main: 'aff3d00',
+      dark: 'ab22a00',
+      contrastText: 'afff'
+    }
+  }
+});
+
+
+        function App() {
+      return (
+<ThemeProvider theme={theme}>
+          <div className="App">
+    <Router>
+        <Navbar />
+        <div className='container'>
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route exact path='/signup' element={<Signup />} />
+        <Route exact path='/login' element={<Login />} />
+        </Routes>
+        </div>
+    </Router>
+  </div>
+        </ThemeProvider>
+       
+      );
+    }
+    
+    export default App;
+
+    */
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,160 @@
-
-
-// home.js
 import React from 'react';
+import { Grid, Typography, Button, makeStyles } from '@material-ui/core';
+import { useTheme, useMediaQuery } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '100%',
+    textAlign: 'center',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  description: {
+    marginBottom: theme.spacing(4),
+    color: '#666',
+  },
+  buttonGroup: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    margin: theme.spacing(0, 1),
+    borderRadius: theme.spacing(3),
+    fontWeight: 'bold',
+    textTransform: 'none',
+    boxShadow: 'none',
+    width: '200px', // Adjust the width as per your preference
+  },
+}));
 
 const Home = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const classes = useStyles();
+
   return (
     <div>
-      <h2>Home</h2>
-      <p>Welcome to the home page!</p>
+      <Grid container spacing={2}>
+        {/* Centered Content */}
+        <Grid item xs={12}>
+          <div className={classes.buttonContainer}>
+            <Typography variant="h4" className={classes.title}>
+              PolisAi
+            </Typography>
+            <Typography variant="body1" className={classes.description}>
+              Transforming AI policy-making through democratic oversight and citizen engagement.
+            </Typography>
+            <div className={classes.buttonGroup}>
+              <Button variant="contained" color="primary" className={classes.button}>
+                Learn more
+              </Button>
+              <Button variant="contained" color="secondary" className={classes.button}>
+                Search
+              </Button>
+            </div>
+          </div>
+        </Grid>
+
+        {/* Text Field Alone (for small screens) */}
+        {isSmallScreen && (
+          <Grid item xs={12}>
+            <Typography variant="h4" className={classes.title}>
+              PolisAi
+            </Typography>
+            <Typography variant="body1" className={classes.description}>
+              Transforming AI policy-making through democratic oversight and citizen engagement.
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
     </div>
   );
 };
 
 export default Home;
+
+
+
+/*
+
+import React from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
+import { useTheme, useMediaQuery, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  image: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+const Home = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container spacing={2}>
+        //{ Left Column }
+        {!isSmallScreen && (
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h4">PolisAi</Typography>
+            <Typography variant="body1">
+            Transforming AI Policy-Making through Democratic Oversight and Citizen Engagement.
+            </Typography>
+          </Grid>
+        )}
+
+  //{ Right Column }
+<Grid item xs={12} sm={6}>
+  <div className={classes.buttonContainer}>
+    <img
+      src={process.env.PUBLIC_URL + '/ai.jpg'}
+      alt="Company Logo"
+      className={classes.image}
+    />
+    <Button variant="contained" color="primary">
+      Button 1
+    </Button>
+    <Button variant="contained" color="secondary">
+      Button 2
+    </Button>
+  </div>
+</Grid>
+
+
+        //{ Text Field Alone (for small screens) }
+        {isSmallScreen && (
+          <Grid item xs={12}>
+            <Typography variant="h4">Company Name</Typography>
+            <Typography variant="body1">
+              Description of the company and its mission.
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+*/
